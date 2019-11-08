fcb4ece90f46c45ff93e43072ebd7a419fd6e202
Borislav Grigorov <bobi@noblehire.io>
2019-11-08 18:12:40 +0200

    use more functional approaches to improve code readability

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,28 +1,66 @@
-import React, {useState, useEffect, useContext, createContext} from 'react';
-import {partial, pipe, filter, join, when, gt} from 'ramda';
-import './styles.css';
+import React, { useState, useEffect, useContext, createContext } from "react";
+import {
+  partial,
+  partialRight,
+  pipe,
+  filter,
+  join,
+  when,
+  ifElse,
+  identity,
+  gt,
+  lt
+} from "ramda";
+import "./styles.css";
 
 ///////////////////
 // --- UTILS --- //
 ///////////////////
 
-function isNotEmptyStr(str) {
-  return str !== '';
+function decrement(i) {
+  return i - 1;
 }
 
-const c = pipe(partial(filter, [isNotEmptyStr]), join(' '));
+function zeroify() {
+  return 0;
+}
+
+function isOutOfTopBoundary(itemsCount) {
+  return partialRight(gt, [itemsCount - 1]);
+}
+
+function isOutOfBottomBoundary() {
+  return partialRight(lt, [0]);
+}
+
+function handleOutOfBottomBoundary() {
+  return ifElse(isOutOfBottomBoundary(), zeroify, identity);
+}
+
+function updateIdxOnItemsCountChange(itemsCount, setIdx) {
+  return when(gt(itemsCount), partial(setIdx, [itemsCount - 1]));
+}
+
+function isNotEmptyStr(str) {
+  return str !== "";
+}
+
+const c = pipe(
+  partial(filter, [isNotEmptyStr]),
+  join(" ")
+);
 
 function classIf(cond, cls) {
-  return !!cond ? cls : ''
+  return !!cond ? cls : "";
 }
 
 ///////////////////
 // --- STATE --- //
 ///////////////////
 
-export const CarouselContext = createContext({idx: 0});
+export const CarouselContext = createContext({ idx: 0 });
 
-export function CarouselProvider({children}) {
+export function CarouselProvider({ children }) {
   const [speed, setSpeed] = useState(500);
   const [idx, setIdx] = useState(0);
   const [itemsCount, setItemsCount] = useState(0);
@@ -30,19 +68,16 @@ export function CarouselProvider({children}) {
   const next = partial(updateIdx, [idx + 1]);
   const prev = partial(updateIdx, [idx - 1]);
 
-  // If the items count changes, reset the idx to an in-range value (the last item).
-  useEffect(() => {
-    when(gt(itemsCount), partial(setIdx, [itemsCount - 1]));
-  }, [itemsCount]);
+  useEffect(partial(updateIdx, [idx]), [itemsCount]);
 
   function updateIdx(nextIdx) {
-    let derived = nextIdx;
-    if (nextIdx < 0) {
-      derived = 0;
-    } else if (nextIdx > (itemsCount - 1)) {
-      derived = itemsCount - 1;
-    }
-    setIdx(derived);
+    const setIdxInRange = ifElse(
+      isOutOfTopBoundary(itemsCount),
+      partial(decrement, [itemsCount]),
+      handleOutOfBottomBoundary()
+    );
+
+    setIdx(setIdxInRange(nextIdx));
   }
 
   const defaultValue = {
@@ -70,31 +105,25 @@ export function useCarousel() {
 // --- CAROUSEL --- //
 //////////////////////
 
-export function Carousel({children, speed}) {
-  const {idx, setItemsCount, setSpeed} = useCarousel();
+export function Carousel({ children, speed }) {
+  const { idx, setItemsCount, setSpeed } = useCarousel();
 
   useEffect(() => setItemsCount(children.length), [children.length]);
   useEffect(() => setSpeed(speed), [speed]);
-
-  function isPrev(i) {
-    return i === idx - 1;
-  }
-
-  function isNext(i) {
-    return i === idx + 1;
-  }
 
   return (
     <div className="carousel">
       <div className="carousel-inner">
         {children.map((child, i) => {
-          const key = i;
+          const isActive = i === idx;
+          const isPrev = i === idx - 1;
+          const isNext = i === idx + 1;
+          const baseCloneProps = { active: isActive, key: i };
+          const cloneProps = isActive
+            ? baseCloneProps
+            : { ...baseCloneProps, isPrev, isNext };
 
-          if (i === idx) {
-            return React.cloneElement(child, {active: true, key});
-          }
-
-          return React.cloneElement(child, {active: false, isPrev: isPrev(i), isNext: isNext(i), key});
+          return React.cloneElement(child, cloneProps);
         })}
       </div>
     </div>
@@ -105,19 +134,19 @@ export function Carousel({children, speed}) {
 // --- SLIDE --- //
 ///////////////////
 
-export function Slide({children, isPrev, isNext, active}) {
-  const {speed} = useCarousel();
+export function Slide({ children, isPrev, isNext, active }) {
+  const { speed } = useCarousel();
   const className = c([
-    'carousel-item',
-    classIf(active, 'carousel-item--active'),
-    classIf(isPrev, 'carousel-item--transition-prev'),
-    classIf(isNext, 'carousel-item--transition-next')
+    "carousel-item",
+    classIf(active, "carousel-item--active"),
+    classIf(isPrev, "carousel-item--transition-prev"),
+    classIf(isNext, "carousel-item--transition-next")
   ]);
   const transitionSpeed = speed / 1000;
   const transition = `transform ${transitionSpeed}s, opacity ${transitionSpeed}s, z-index ${transitionSpeed}s`;
 
   return (
-    <div className={className} style={{transition}}>
+    <div className={className} style={{ transition }}>
       {children}
     </div>
   );
